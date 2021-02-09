27b525c4427aa1166d5cbbaabae6da254d9568d8
d0hyeon <ehgus6887@gmail.com>
2021-02-09 23:31:30 +0900

    style: 로그인 폼 컴포넌트 퍼블리싱 완료

---
+++ src/components/common/LoginForm.svelte
@@ -0,0 +1,149 @@
+<script type="ts">
+  import {goto} from '@sapper/app';
+
+  import Input from '~components/inputs/Input.svelte';
+
+  type OnSuccess = (session: any) => void;
+  
+  export const onSuccess:OnSuccess = () => {
+    goto('/index');
+  }
+
+  let username: string = '';
+  let password: string = '';
+  let isShowPassword: boolean = false;
+
+  $: handleClickPwIcon = () => {
+    isShowPassword = !isShowPassword;
+  }
+
+  $: {
+    console.log(username);
+  }
+
+  
+</script>
+
+<div class="login-form">
+  <div class="container">
+    <h1>Dostagram</h1>
+    <form>
+      <ul class="fields">
+        <li>
+          <Input bind:value={username} placeholder="전화번호, 사용자 이름 또는 이메일" />
+        </li>
+        <li>
+          <Input bind:value={password} type={isShowPassword ? 'text' : 'password'} placeholder="비밀번호" >
+            {#if password}
+              <button type="button" class="input-icon" on:click={handleClickPwIcon}>비밀번호 {isShowPassword ? '숨기기' : '표시'}</button>
+            {/if}
+          </Input>
+        </li>
+        <li class="field-button">
+          <button type="submit" disabled={!(username || password)}>로그인</button>
+        </li>
+      </ul>
+      <div class="line">
+        <p>또는</p>
+      </div>
+      <ul class="links">
+        <li>
+          <a href="/find">비밀번호를 잊으셨나요?</a>
+        </li>
+      </ul>
+    </form>
+  </div>
+  <div class="container">
+    <p class="text-join">
+      <span>계정이 없으신가요?</span> 
+      <em>
+        <a href="/signup">가입하기</a>
+      </em>
+    </p>
+  </div>
+</div>
+
+
+<style lang="scss">
+  .container {
+    padding: 10px;
+    border: 1px solid #ddd;
+    background-color: #fff;
+    text-align: center;
+
+    & ~ & {
+      margin-top: 10px;
+    }
+  }
+
+  .login-form {
+    h1 {
+      width: 105px;
+      height: 47px;
+      background-repeat: no-repeat;
+      background-image: url('/images/icon/logo105x47.png');
+      margin: 22px auto 12px;
+      font-size: 0;
+    } 
+    .fields {
+      > li {
+        margin: 0 40px 6px;
+
+        &.field-button {
+          padding-top: 8px;
+        }
+      }
+
+      .input-icon {
+        white-space: nowrap;
+      }
+    } 
+
+    button[type=submit] {
+      width: 100%;
+      height: 30px;
+      background-color: #0095f6;
+      border-radius: 4px;
+      color: #fff;
+
+      &:disabled {
+        opacity: 0.4;
+      }
+    }
+   
+    .line {
+      display: flex;
+      margin: auto 40px;
+      padding: 10px 0;
+      align-items: center;
+
+      &::before, &::after {
+        display: block;
+        height: 1px;
+        width: 100%;
+        background-color: #ddd;
+        content: '';
+      }
+      
+      p {
+        flex: 1 0 auto;
+        margin: 0 18px;
+      }
+    }
+
+    .links {
+      padding-top: 8px;
+    }
+  }
+
+  .text-join {
+    margin: 15px;
+    font-size: 14px;
+    
+    em {
+      color: #0095f6;
+      vertical-align: middle;
+      font-weight: bold;
+    }
+  }
+</style>
