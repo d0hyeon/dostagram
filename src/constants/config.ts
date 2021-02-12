6134c8ca991f935840dd7681148c4e493afbbbaf
d0hyeon <ehgus6887@gmail.com>
2021-02-12 15:20:28 +0900

    feat: firebase config 상수 추가

---
+++ src/constants/config.ts
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: process.env.FireBaseAPIKey,
+  authDomain: process.env.FireBaseAuthDomain,
+  projectId: process.env.FireBaseProductId,
+  storageBucket: process.env.FireBaseStorageBucket,
+  messagingSenderId: process.env.FireBaseMessagingSenderId,
+  appId: process.env.FireBaseAppId,
+  measurementId: process.env.FireBaseMeasurementid,
+};
+
