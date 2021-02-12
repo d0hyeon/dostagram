a6e02cf6cfbdec15de26057aba357f6d844067db
d0hyeon <ehgus6887@gmail.com>
2021-02-12 22:37:20 +0900

    feat: 헤더 네비게이션 컴포넌트 추가

---
+++ src/components/layout/header/Nav.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  import HomeIcon from '~components/icons/Home.svelte';
+	export let segment: string;
+</script>
+
+<nav>
+	<ul>
+		<li>
+      <a href=".">
+        <HomeIcon isActive={segment === undefined} />
+      </a>
+    </li>
+	</ul>
+</nav>
+
+<style>
+	ul {
+		display: flex;
+	}
+
+  li {
+    margin-left: 22px;
+  }
+
+</style>
