1a7233eaa5699adb6ef036217203de1ce6c6c67d
d0hyeon <ehgus6887@gmail.com>
2021-02-12 15:22:12 +0900

    feat: 에픽 컴포넌트 마운트

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,5 +1,16 @@
+<script>
+import Epic from "src/components/Epic.svelte";
 
-
+</script>
 <main>
+  <Epic />
 	<slot></slot>
 </main>
+
+<style lang="scss">
+	main {
+    width: 100%;
+    height: 100vh;
+    background-color: var(--background-color);
+  }
+</style>
